1604cd9a402809e94c00080fb4d13a06c0d96c9e
Toon Borgers <borgers.toon@gmail.com>
2017-01-24 07:46:00 +0100

    Added async pipe for getContacts()

--- src/app/contacts-list/contacts-list.component.ts
+++ src/app/contacts-list/contacts-list.component.ts
@@ -2,20 +2,21 @@ import {Component, OnInit} from '@angular/core';
 import {ContactsService} from "../contacts.service";
 import {Contact} from "../models/contact";
 
+import {Observable} from "rxjs/Observable";
+
 @Component({
   selector: 'trm-contacts-list',
   templateUrl: './contacts-list.component.html',
   styleUrls: ['./contacts-list.component.css']
 })
 export class ContactsListComponent implements OnInit {
-  private contacts: Array<Contact> = [];
+  private contacts: Observable<Array<Contact>>;
 
   constructor(private contactsService: ContactsService) {
   }
 
   ngOnInit() {
-    this.contactsService.getContacts()
-      .subscribe(contacts => this.contacts = contacts);
+    this.contacts = this.contactsService.getContacts();
   }
 
 }
