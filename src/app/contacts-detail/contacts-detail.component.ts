82fd18bad0e63948a6507811ef2a181e1419735a
Toon Borgers <borgers.toon@gmail.com>
2017-01-24 14:05:18 +0100

    Use buttons istead of anchors

--- src/app/contacts-detail/contacts-detail.component.ts
+++ src/app/contacts-detail/contacts-detail.component.ts
@@ -10,6 +10,7 @@ import {Contact} from "../models/contact";
 })
 export class ContactsDetailComponent {
   @Input() contact: Contact;
+
   @Output() edit = new EventEmitter<any>();
   @Output() back = new EventEmitter<any>();
 }
